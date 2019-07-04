e58a849afa740b75c35cd223d796ac1e0b55ad30
Jefferson Ribeiro <43348822+jeffersribeiro@users.noreply.github.com>
2019-07-04 19:38:53 -0300

    commit

--- index.js
+++ index.js
@@ -23,29 +23,111 @@
     })
 
     function createTask(taskName, description) {
+        counter += 1
         let tasks = {
-            taskName: task,
             description: description,
-            id: counter
+            task: taskName,
+            id: counter,
         }
-        var db = firebase.database().ref('taks/' + counter)
-        db.set(tasks)
+        let db = firebase.database().ref('tasks/' + counter)
+        db.set(tasks);
+        document.getElementById('cardSection').innerHTML = ''
+        readTask();
     };
+
 
     function readTask() {
         var task = firebase.database().ref('tasks/')
         task.on('child_added', function(data) {
             let taskValue = data.val()
-            console.log(taskValue)
             document.getElementById('cardSection').innerHTML += `
-         <div class='card mb-3'>
-         <div class='card-body'>
-         <h5 class='card-title'>${taskValue.task}</h5>
-         <p class='card-text'>${taskValue.description}</p>
-         <button type='submit' style='color:white' class='btn btn-warning'
-         onclick='updatedTask(${taskValue.id},${taskValue.task},${taskValue.description})'>Edit Task</button>
-         </div>
-         </div>
-         `
+            <div class='card mb-3'>
+            <div class='card-body'>
+            <h5 class='card-title'>${taskValue.task}</h5>
+            <p class='card-text'>${taskValue.description}</p>
+           <button type='submit' style='color:white' class='btn btn-warning'
+            onclick="updateTask(${taskValue.id},'${taskValue.task}','${taskValue.description}')">Edit Task</button>
+            <button type='submit' class='btn btn-danger' onclick='deleteTask(${taskValue.id})'>Delete</button>
+            </div>
+            </div>`
         })
+    }
+
+    function reset() {
+        document.getElementById('fistSection').innerHTML = `
+        <form class="border p-4" id="form">
+            <div class="form-group">
+                <label>Task</label>
+                <input type="text" class="form-control" id="task" placeholder="Enter Task">
+            </div>
+            <div class="form-group">
+                <label>Description</label>
+                <input type="text" class="form-control" id="description" placeholder="Description">
+            </div>
+
+            <button id="button1" class="btn btn-primary">ADD Task</button>
+            <button style="display: none" type="submit" id="button2" class="btn btn-success">Update Task</button>
+            <button style="display: none" type="submit" id="button3" class="btn btn-danger">Cancell</button>
+        </form>`;
+        document.getElementById('form').addEventListener('submit', (e) => {
+            var task = document.getElementById('task').value;
+            var description = document.getElementById('description').value;
+            e.preventDefault();
+            createTask(task, description)
+            form.reset();
+        })
+    }
+
+    function updateTask(id, name, description) {
+        document.getElementById('fistSection').innerHTML = ` 
+        <form class="border p-4" id="form2">
+
+        <div class="form-group">
+            <label>Task</label>
+            <input type="text" class="form-control" id="task" placeholder="Enter Task">
+        </div>
+        
+        <div class="form-group">
+            <label>Description</label>
+            <input type="text" class="form-control" id="description" placeholder="Description">
+        </div>
+
+        <button style="display: none"  id="button1" class="btn btn-primary">ADD Task</button>
+        <button style="display: inline-block" type="submit" id="button2" class="btn btn-success">Update Task</button>
+        <button style="display: inline-block"  id="button3" class="btn btn-danger">Cancell</button>
+    </form>`;
+        document.getElementById('form2').addEventListener('submit', (e) => {
+            e.preventDefault();
+        });
+        document.getElementById('button3').addEventListener("click", (e) => {
+            reset()
+        });
+        document.getElementById('button2').addEventListener("click", (e) => {
+            updateTask2(id, document.getElementById('task').value, document.getElementById('description').value)
+        });
+        document.getElementById('task').value = name;
+        document.getElementById('description').value = description;
+
+    };
+
+    function updateTask2(id, name, description) {
+        var taskUpdated = {
+            task: name,
+            id: id,
+            description: description
+        }
+        let db = firebase.database().ref('tasks/' + id)
+        db.set(taskUpdated);
+
+        document.getElementById('cardSection').innerHTML = '',
+            readTask();
+        reset();
+    }
+
+    function deleteTask(id) {
+        var task = firebase.database().ref("tasks/" + id);
+        task.remove();
+        reset();
+        document.getElementById('cardsection').innerHTML = '';
+        readTask();
     }
